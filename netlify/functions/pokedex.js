d4f83db82fe509cdcc386b4edacdcbaaa52c060b
Cynthia Teeters <cynthiateeters@gmail.com>
2022-04-15 22:37:22 -0400

    Fix multiple issues

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,29 +1,29 @@
-const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const chalk = require('chalk');
-const { DateTime } = require('luxon');
+const { DateTime } = require("luxon");
+
+/*
+https://github.com/node-fetch/node-fetch/blob/HEAD/docs/v3-UPGRADE-GUIDE.md#converted-to-es-module
+*/
 
 exports.handler = async function (event, context) {
-  const date = DateTime.now();
+
+ const date = DateTime.now();
 
   const eventBody = JSON.parse(event.body);
-  console.log(chalk.cyan('#40CEFF')(`${date}`));
-  console.log(chalk.cyan(`\teventBody.region: ${eventBody.region} `));
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
+  console.log(chalk.magenta(`${date}`));
+  console.log(chalk.magenta(`\teventBody.region: ${eventBody.region}`));
 
-  console.log(event);
+  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
 
   const response = await fetch(POKE_API);
   const data = await response.json();
 
-  console.log(
-    chalk.cyan(`\tNumber of entires: ${data.pokemon_entries.length}`)
-  );
+  console.log(chalk.magenta(`\tNumber of entries: ${data.pokemon_entries.length}`));
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      pokemon: data.pokemon_entries,
-    }),
-  };
-};
+    body: JSON.stringify(data.pokemon_entries)
+  }
+}
+
