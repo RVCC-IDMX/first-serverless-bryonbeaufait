45a577ebc23fea6c799c215e2ff5511614ff7b97
Bryon <beaufaitb@gmail.com>
2022-04-15 21:05:42 -0400

    add chalk

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,14 +1,24 @@
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
 
 exports.handler = async function (event, context) {
+  const date = DateTime.now();
+
   const eventBody = JSON.parse(event.body);
+  console.log(chalk.cyan('#40CEFF')(`${date}`));
+  console.log(chalk.cyan(`\teventBody.region: ${eventBody.region} `));
   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region;
 
   console.log(event);
 
   const response = await fetch(POKE_API);
   const data = await response.json();
+
+  console.log(
+    chalk.cyan(`\tNumber of entires: ${data.pokemon_entries.length}`)
+  );
 
   return {
     statusCode: 200,
